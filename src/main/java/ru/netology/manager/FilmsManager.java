24c3973efed7611f8036a888cd9524368faf83e7
Aleksandr <artemyev86an@gmail.com>
2022-06-23 16:18:34 +0500

    PosterStart

---
+++ src/main/java/ru/netology/manager/FilmsManager.java
@@ -0,0 +1,44 @@
+package ru.netology.manager;
+
+public class FilmsManager {
+    private String[] movies = new String[0];
+    private  int limit;
+
+    public FilmsManager() {  // limit = 10
+        limit = 10;
+    }
+
+    public FilmsManager(int limit) {
+        this.limit = limit;
+
+    }
+
+    public void add(String film) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = film;
+        movies = tmp;
+
+    }
+
+    public String[] findAll() {
+        return movies;
+
+
+    }
+
+    public String[] findLast() {
+        String[] ans;
+        if (limit < movies.length) {
+            ans = new String[limit];
+        } else {
+            ans = new String[movies.length];
+        }
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = movies[movies.length - i];
+        }
+        return  ans;
+    }
+}
